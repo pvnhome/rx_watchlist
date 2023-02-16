c8fb8513b22e4bd0489b083ddb6769b6097bbe0d
Victor Pyankov <v.pyankov@kase.kz>
2023-02-16 19:21:48 +0600

    В классе Pull отобразается список наблюдения по ценным бумагам

--- src/main/java/kz/kase/examples/Pull.java
+++ src/main/java/kz/kase/examples/Pull.java
@@ -1,5 +1,7 @@
 package kz.kase.examples;
 
+import java.util.List;
+
 import kz.bips.comps.utils.DateUtils;
 import kz.bips.comps.utils.ExtendedPreferencesHocon;
 import kz.bips.comps.utils.Log4JLoggerWrapper;
@@ -8,6 +10,7 @@ import kz.kase.iris.client.connectors.paho.PahoConnector;
 import kz.kase.iris.client.rx.IrisRxClient;
 import kz.kase.iris.model.IrisApiBase.Ohlc;
 import kz.kase.iris.model.IrisApiCurtotals.CurTotal;
+import kz.kase.iris.model.IrisApiTotals.Total;
 import kz.kase.iris.model.IrisApiWatchlist.WatchlistInstrumentType;
 import kz.kase.iris.model.IrisApiWatchlist.WatchlistRequest;
 import kz.kase.iris.utils.IrisApiUtils;
@@ -29,17 +32,30 @@ public class Pull {
       try (IrisRxClient client = new IrisRxClient(new PahoConnector(props))) {
          t = log.time("open", start);
 
-         // Формируем запрос на список наблюдения. В качестве оптимизации ограничиваем возвращаемый результат валютными инструментами.
-         WatchlistRequest request = WatchlistRequest.newBuilder().addTypes(WatchlistInstrumentType.WIT_CURRENCIES).build();
+         // Формируем запрос на список наблюдения. В качестве оптимизации ограничиваем возвращаемый результат валютными инструментами и ценными бумагами.
+         WatchlistRequest request = WatchlistRequest.newBuilder().addTypes(WatchlistInstrumentType.WIT_CURRENCIES).addTypes(WatchlistInstrumentType.WIT_SECURITIES).build();
 
          // Делаем запрос на поучение списка наблюдения пользователя.
          client.getWatchlistTopic().send(request).blockingSubscribe(reply -> {
-            log.debug("WATCHLIST:");
-            for (CurTotal total : reply.getCurQuotesList()) {
-               Ohlc price = total.getPrice();
-               String cp = price.hasClose() ? IrisApiUtils.fromDecimal(price.getClose()).toPlainString() : "цена неизвестна";
-               String ct = price.hasCloseTime() ? IrisApiUtils.toLocalDateTime(price.getCloseTime()).format(DateUtils.DDMMYYYY_TIME) : "сегодня не торговался";
-               log.debug("   %s (%s): %s", total.getInstrumentCode(), ct, cp);
+            List<CurTotal> curQuotesList = reply.getCurQuotesList();
+            if (!curQuotesList.isEmpty()) {
+               log.debug("WATCHLIST (CURRENCIES):");
+               for (CurTotal total : curQuotesList) {
+                  Ohlc price = total.getPrice();
+                  String cp = price.hasClose() ? IrisApiUtils.fromDecimal(price.getClose()).toPlainString() : "цена неизвестна";
+                  String ct = price.hasCloseTime() ? IrisApiUtils.toLocalDateTime(price.getCloseTime()).format(DateUtils.DDMMYYYY_TIME) : "сегодня не торговался";
+                  log.debug("   %s (%s): %s", total.getInstrumentCode(), ct, cp);
+               }
+            }
+            List<Total> secQuotesList = reply.getSecQuotesList();
+            if (!secQuotesList.isEmpty()) {
+               log.debug("WATCHLIST (SECURITIES):");
+               for (Total total : secQuotesList) {
+                  Ohlc price = total.getPrice();
+                  String cp = price.hasClose() ? IrisApiUtils.fromDecimal(price.getClose()).toPlainString() : "цена неизвестна";
+                  String ct = price.hasCloseTime() ? IrisApiUtils.toLocalDateTime(price.getCloseTime()).format(DateUtils.DDMMYYYY_TIME) : "сегодня не торговался";
+                  log.debug("   %s (%s): %s", total.getInstrumentCode(), ct, cp);
+               }
             }
          }, error -> {
             log.logStackTrace(error, "Watchlist request");
